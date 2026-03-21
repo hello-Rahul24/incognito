a5ce2b1e75e43660307f9762c2feeb3df1ce91c1
Rahul Giri <girirahul2028@gmail.com>
2026-03-21 09:33:35 +0530

    v-0.0.0.5

--- app/api/room/join/route.ts
+++ app/api/room/join/route.ts
@@ -7,27 +7,34 @@ const roomIdSchema = z.object({
     roomId : z.string()
 })
 
+// JOIN ROUTE WILL ACCEPT THE ROOMCODE
+
 export async function  POST(req: Request){
     //user will add room id
-
     const data = await req.json();
+    //checking from zod schema
     const parsedData = roomIdSchema.safeParse(data);
+    //if wrong input then return error
     if(!parsedData.success){
         return Response.json({
             success: false,
             message: "we asked for roomID dont add anything"
         })
     }
+    // otherwise get roomId
     const roomId = parsedData.data?.roomId;
+    //check in redis database is it availabel or not
     const room: Room|null = await redis.get(`room:${roomId}`);
-    //if exist then add
+    //if not exist then return error
     if(!room){
         return Response.json({
             success: false,
             message: "this room doesnt exist"
         })
     }
+    //if exist then generate name 
     const alias = generateName();
+    //generate the member
     const member: Member = {
         isHost: false,
         alias: alias
@@ -36,6 +43,7 @@ export async function  POST(req: Request){
     room.members.push(member);
     //push again the room to redis
     await redis.set(`room:${roomId}`, room);
+
     return Response.json({
         success: true,
         message: `you are joined to ${roomId}`,
