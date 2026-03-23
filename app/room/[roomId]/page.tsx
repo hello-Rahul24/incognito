e1d650b4b3a4a22c86378fe2ac72361c8ef0846d
Rahul Giri <girirahul2028@gmail.com>
2026-03-24 02:36:34 +0530

    v-0.0.1.6

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -2,6 +2,8 @@
 
 import { aliasGet, roomIdGet } from "@/lib/session";
 import { Message } from "@/types/room";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
 export default function Room() {
@@ -9,83 +11,103 @@ export default function Room() {
   const [alias, setAlias] = useState<string | null>("");
   const [roomId, setRoomId] = useState<string | null>("");
   const [inputMessage, setInputMessage] = useState<string>("");
-  const socketRef = useRef<WebSocket|null>(null);
   //const [isHost, setIsHost] = useState<string | null>("false");
+  const socketRef = useRef<WebSocket | null>(null);
+
+  const router = useRouter();
+  const params = useParams();
+  const roomIdParam = params.roomId;
 
   useEffect(() => {
     const alias = aliasGet();
     const roomId = roomIdGet();
     //const isHost = isHostGet();
+    
+    //route protection
+    if (!alias || !roomId) {
+      router.push("/");
+    }
+    if (roomId != roomIdParam) {
+      router.push("/");
+    }
     setAlias(alias);
     setRoomId(roomId);
     //setIsHost(isHost);
   }, []);
   //connect to websocket server
-  useEffect(()=> {
+  useEffect(() => {
     if (!roomId) {
       return;
     }
     const socket = new WebSocket(`ws://localhost:8080?roomId=${roomId}`);
     socketRef.current = socket;
-    socket.onopen = ()=>{
-        socket.send(JSON.stringify({type: "JOIN", "alias": `${alias}`}));
-    }
+    socket.onopen = () => {
+      socket.send(JSON.stringify({ type: "JOIN", alias: `${alias}` }));
+    };
 
-    socket.onmessage = (event)=> {
-        const data = JSON.parse(event.data);
-        if(data.type === "MESSAGE"){
-            setMessage((prev)=>[...prev, data.payload]);
-        }
-    }
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.type === "MESSAGE") {
+        setMessage((prev) => [...prev, data.payload]);
+      }
+    };
     //cleanup logic
-    return ()=> {
-        socket.close();
-    }
-  },[roomId, alias]);
+    return () => {
+      socket.close();
+    };
+  }, [roomId, alias]);
   //restore the messages
   useEffect(() => {
     async function fetchMsg() {
-        if(roomId){
-            const res =await fetch(`/api/room/${roomId}`);
+      if (roomId) {
+        const res = await fetch(`/api/room/${roomId}`);
         const data = await res.json();
         console.log(data);
         setMessage(data.messages || []);
-        }
-        
+      }
     }
     fetchMsg();
-  },[roomId]);
+  }, [roomId]);
 
-  function handelInputBox(){
-    socketRef.current?.send(JSON.stringify({
+  function handelInputBox() {
+    socketRef.current?.send(
+      JSON.stringify({
         type: "MESSAGE",
         payload: {
-            content: inputMessage,
-            sentAt: new Date().toISOString(),
-            sentby: alias
-        }
-    }));
+          content: inputMessage,
+          sentAt: new Date().toISOString(),
+          sentby: alias,
+        },
+      })
+    );
     setInputMessage("");
   }
 
   return (
     <>
-    <div>
+      <div>
         <div className="bg-gray-700 text-4xl">inbox</div>
         <div className="bg-amber-100 h-80">
-            {message.map((v,id)=>{
-                return <div key={id}>
-                    {v.content}
-                    {v.sentby}
-                    {v.sentAt}
-                </div>
-            })}
+          {message.map((v, id) => {
+            return (
+              <div key={id}>
+                {v.content}
+                {v.sentby}
+                {v.sentAt}
+              </div>
+            );
+          })}
         </div>
         <div className="bg-gray-700">
-            <input onChange={(e)=>setInputMessage(e.target.value)} type="text" placeholder="type your message" className="bg-amber-400"/>
-            <button onClick={handelInputBox}>Send</button>
+          <input
+            onChange={(e) => setInputMessage(e.target.value)}
+            type="text"
+            placeholder="type your message"
+            className="bg-amber-400"
+          />
+          <button onClick={handelInputBox}>Send</button>
         </div>
-    </div>
+      </div>
     </>
   );
 }
